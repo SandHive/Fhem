930336adf0fede5ae26536f555ba0c3f88d052f8
Sandman <sandhive@gmail.com>
2017-01-16 10:27:01 +0100

    Project "Sand.Fhem.Home.Basics" added

---
+++ Home/Basics/Services/ApplicationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+//-----------------------------------------------------------------------------
+namespace Sand.Fhem.Home.Basics.Services
+{
+    public class ApplicationService : IApplicationService
+    {
+        //---------------------------------------------------------------------
+        #region Fields
+
+        private Dispatcher  m_dispatcher = Application.Current.Dispatcher;
+
+        //-- Fields
+        #endregion
+        //---------------------------------------------------------------------
+        #region IApplicationService Members
+
+        public void ShowErrorMessage( string a_errorText )
+        {
+            //-- Show the error information
+            MessageBox.Show( a_errorText, "Error", MessageBoxButton.OK, MessageBoxImage.Error );
+        }
+
+        [DebuggerStepThrough]
+        public void ShowErrorMessage( Exception a_exception )
+        {
+            this.ShowErrorMessage( a_exception.Message );
+        }
+
+        public Dispatcher UserInterfaceDispatcher
+        {
+            get { return m_dispatcher; } }
+
+        //-- IApplicationService Members
+        #endregion
+        //---------------------------------------------------------------------
+    }
+}
+//-----------------------------------------------------------------------------
---
+++ Home/Basics/Services/IApplicationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Threading;
+//-----------------------------------------------------------------------------
+namespace Sand.Fhem.Home.Basics.Services
+{
+    public interface IApplicationService
+    {
+        /// <summary>
+        /// Shows an error message.
+        /// </summary>
+        /// <param name="a_exception">
+        /// The exception whose message text should be shown.
+        /// </param>
+        void ShowErrorMessage( Exception a_exception );
+
+        /// <summary>
+        /// Shows an error message.
+        /// </summary>
+        /// <param name="a_errorText">
+        /// The error text that should be shown.
+        /// </param>
+        void ShowErrorMessage( string a_errorText );
+
+        /// <summary>
+        /// Gets the dispatcher for invoking into the UI thread.
+        /// </summary>
+        Dispatcher UserInterfaceDispatcher { get; }
+    }
+}
+//-----------------------------------------------------------------------------
